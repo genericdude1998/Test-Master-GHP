747b7edcf1b6de637b714f1de53d95808ef41541
vsenzo <vsenzo@hello.com>
2021-11-01 11:24:50 +0000

    changed

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>Hello I have develted the build folder hellookopdcdnvdnvfd!</h1>, document.getElementById('root'));
+ReactDOM.render(<h1>Hello I have develted the build folder hellookopdcdnvdnvfd my goodness!</h1>, document.getElementById('root'));
