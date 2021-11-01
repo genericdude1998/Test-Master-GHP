c8bd67932eae5a77a80cc7ca7d83672a265d2d16
vsenzo <vsenzo@hello.com>
2021-11-01 10:47:47 +0000

    new changes coming

--- build/bundle.js
+++ build/bundle.js
@@ -16,7 +16,7 @@
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n/* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-dom */ \"./node_modules/react-dom/index.js\");\n\n\nreact_dom__WEBPACK_IMPORTED_MODULE_1__.render( /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(\"h1\", null, \"Helloworld Github Pages I have changed again and agai and again\"), document.getElementById('root'));\n\n//# sourceURL=webpack://test-ghp-app/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n/* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-dom */ \"./node_modules/react-dom/index.js\");\n\n\nreact_dom__WEBPACK_IMPORTED_MODULE_1__.render( /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(\"h1\", null, \"Hello new Git\"), document.getElementById('root'));\n\n//# sourceURL=webpack://test-ghp-app/./src/index.js?");
 
 /***/ }),
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>Hello new Git</h1>, document.getElementById('root'));
+ReactDOM.render(<h1>Hello new Git this has been changed!</h1>, document.getElementById('root'));
