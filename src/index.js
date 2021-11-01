b71cb57c4478c7f063e8300fc521d60054d44b9e
vsenzo <vsenzo@hello.com>
2021-11-01 10:45:50 +0000

    Changes index

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>Helloworld Github Pages I have changed again and agai and again</h1>, document.getElementById('root'));
+ReactDOM.render(<h1>Hello new Git</h1>, document.getElementById('root'));
